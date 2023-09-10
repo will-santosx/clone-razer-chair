9714928c4f418366867b6a54268d51c4f263bf5e
Willians Santos <taldowill29@gmail.com>
2023-09-10 13:51:33 -0400

    adicionando novas fontes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,10 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				RazerF5: "RazerF5,Tahoma,Arial,sans-serif;",
-				normal: "Roboto,helvetica neue,Arial,noto sans,sans-serif",
+				RazerF5: "RazerF5, Tahoma, Arial, sans-serif;",
+				RazerLight: "RazerF5Light, Arial, Helvetica, sans-serif;",
+				RazerBold: "RazerF5Bold, Arial, Tahoma, sans-serif;",
+				Normal: "Roboto, helvetica neue, Arial, noto sans, sans-serif;",
 			},
 		},
 	},
